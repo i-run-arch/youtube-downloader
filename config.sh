fb16f8b9c01aa6f22a85a3288a388a194149bb74
gabosxpiens <53384203+gabosxpiens@users.noreply.github.com>
2020-01-06 23:27:10 -0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -11,7 +11,7 @@ echo "configuring youtube-dl"
 mkdir ~/storage/shared/Youtube-downloads &> /dev/null
 mkdir -p ~/.config/youtube-dl &> /dev/null
 touch ~/.config/youtube-dl/config &> /dev/null
-echo '--no-mtime -o /data/data/com.termux/files/home/storage/shared/Youtube-downloads/%(title)s.%(ext)s-f"best[height<=1080]".avi' >> ~/.config/youtube-dl/config &> /dev/null
+echo '--no-mtime -o /data/data/com.termux/files/home/storage/shared/Youtube-downloads/%(title)s.%(ext)s -f "best[height<=1080]".avi' >> ~/.config/youtube-dl/config &> /dev/null
 mkdir ~/bin &> /dev/null
 touch ~/bin/termux-url-opener &> /dev/null 
 echo 'youtube-dl $1' >> ~/bin/termux-url-opener &> /dev/null
