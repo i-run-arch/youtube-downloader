b0717ee473f5dc3ba92482d6cd225e9408269f68
gabosxpiens <53384203+gabosxpiens@users.noreply.github.com>
2020-01-06 22:51:16 -0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,11 +1,15 @@
-termux-setup-storage
-apt update && apt upgrade -y
-pkg install python -y
-pip install youtube-dl
-mkdir ~/storage/shared/Youtube
-mkdir -p ~/.config/youtube-dl
-touch ~/.config/youtube-dl/config
-echo ' --no-mtime -o /data/data/com.termux/files/home/storage/shared/Youtube/%(title)s.%(ext)s-f"best[height>=1080]".avi' >> ~/.config/youtube-dl/config
-mkdir ~/bin
-touch ~/bin/termux-url-opener
-echo 'youtube-dl $1'
+termux-setup-storage &> /dev/null
+apt update && apt upgrade -y &> /dev/null
+pkg install python -y &> /dev/null
+echo "installing youtube-dl"
+pip install youtube-dl &> /dev/null 
+echo "configuring youtube-dl"
+mkdir ~/storage/shared/Youtube-downloads &> /dev/null
+mkdir -p ~/.config/youtube-dl &> /dev/null
+touch ~/.config/youtube-dl/config &> /dev/null
+echo ' --no-mtime -o /data/data/com.termux/files/home/storage/shared/Youtube-downloads/%(title)s.%(ext)s-f"best[height>=1080]".avi' >> ~/.config/youtube-dl/config &> /dev/null
+mkdir ~/bin &> /dev/null
+touch ~/bin/termux-url-opener &> /dev/null 
+echo 'youtube-dl $1' >> ~/bin/termux-url-opener &> /dev/null
+echo "youtube-dl configured"
+
