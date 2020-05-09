33bf8fc3a0dbfc32b9242a930a469e19f98533bc
i-use-BSD <pholbrook@fundamentalbaptistchurch.site>
2020-05-09 07:18:57 -0300

    add tangled script

--- config.sh
+++ config.sh
@@ -1,35 +1,29 @@
-termux-setup-storage 
-#Asks for storage permissions
+termux-setup-storage
 
 sleep 5s
-apt update && apt upgrade -y 
-#Updates and upgrades termux pakages
+apt update && apt upgrade -y
+pkg install wget curl python ffmpeg -y
+pip install youtube-dl
 
-echo "..."
-echo "installing python "
-pkg install python -y 
-#Installs python
 
-echo "..."
-echo "installing youtube-dl"
-pip install youtube-dl  
-#Installs youtube-dl
+if [[ ! -d /data/data/com.termux/files/home/storage/shared/Youtube ]]; then
+    mkdir -p /data/data/com.termux/files/home/storage/shared/Youtube
+fi
+if [[ ! -d ~/.config/youtube-dl ]]; then
+    mkdir -p ~/.config/youtube-dl
+fi
 
-echo "..."
-echo "configuring youtube-dl"
-mkdir /data/data/com.termux/files/home/storage/shared/Youtube-downloads 
-#Creates folder where the videos will be downloaded
+cat >~/.config/youtube-dl <<EOF
+  -o /data/data/com.termux/files/home/storage/shared/Youtube-downloads/%(title)s.%(ext)s -f "best[height<=1080]"
+EOF
 
-mkdir -p ~/.config/youtube-dl  
-#Creates youtube-dl config folder
-
-echo '--no-mtime -o /data/data/com.termux/files/home/storage/shared/Youtube-downloads/%(title)s.%(ext)s -f "best[height<=1080]"' >> ~/.config/youtube-dl/config 
+echo '--no-mtime -o /data/data/com.termux/files/home/storage/shared/Youtube-downloads/%(title)s.%(ext)s -f "best[height<=1080]"' >> ~/.config/youtube-dl/config
 #Creates config file for youtube-dl in "[height<=1080]" 1080 can be replace for any other resolution, this will set the maximum resolution available that will be downloaded
 
-mkdir ~/bin  
+mkdir ~/bin
 #Creates bin foler for termux-url-opener
 
-echo 'youtube-dl $1' >> ~/bin/termux-url-opener 
+echo 'youtube-dl $1' >> ~/bin/termux-url-opener
 #Creates file to open youtube links by directly sharing with termux app
 
 cd ~
